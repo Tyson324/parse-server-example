710c463b9883deae2075dc46df15996cea2ce256
Tyson324 <sgonez@hotmail.com>
2017-01-27 14:48:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://spadmin:Shereen3.141592653@ds054999.mlab.com:54999/spmdb0420',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  appId: process.env.APP_ID || 'gRgG6rXH59W6J19MrqWtiAWAdEGqjoZjEs0GsH6H',
+  masterKey: process.env.MASTER_KEY || '4aiQUtxo5fAHgbgle2a4EafsS1p7kkTks4VvBmpZ', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://mssignpost.azurewebsites.net/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
